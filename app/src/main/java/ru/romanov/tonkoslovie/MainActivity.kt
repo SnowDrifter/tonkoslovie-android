060192215b8614bdd8a107872632bc471b05587c
Ivan <fl125@yandex.ru>
2018-04-07 04:20:02 +0300

    Add basic realization for left side menu

--- app/src/main/java/ru/romanov/tonkoslovie/MainActivity.kt
+++ app/src/main/java/ru/romanov/tonkoslovie/MainActivity.kt
@@ -1,12 +1,82 @@
 package ru.romanov.tonkoslovie
 
-import android.support.v7.app.AppCompatActivity
+import android.app.AlertDialog
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        nav_view.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
     }
+
+    fun sendMessage(view: View) {
+        val builder = AlertDialog.Builder(this@MainActivity)
+        builder.setTitle("Заголовок")
+                .setMessage("Текст")
+                .setCancelable(false)
+                .setNegativeButton("ОК!",
+                        { dialog, _ -> dialog.cancel() })
+        val alert = builder.create()
+        alert.show()
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_settings -> {
+                Toast.makeText(applicationContext, "Настройки", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_lessons -> {
+                Toast.makeText(applicationContext, "Уроки", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_themes -> {
+                Toast.makeText(applicationContext, "Темы", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_settings -> {
+                Toast.makeText(applicationContext, "Настройки", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
 }
