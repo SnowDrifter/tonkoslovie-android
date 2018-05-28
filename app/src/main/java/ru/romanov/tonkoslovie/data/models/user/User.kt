4e471fd0c482dcdd23df098bc91c923ff4bdbae9
Ivan <fl125@yandex.ru>
2018-05-29 02:40:26 +0300

    Added username field to profile activity

--- app/src/main/java/ru/romanov/tonkoslovie/data/models/user/User.kt
+++ app/src/main/java/ru/romanov/tonkoslovie/data/models/user/User.kt
@@ -1,7 +1,6 @@
 package ru.romanov.tonkoslovie.data.models.user
 
 data class User(
-        val id: Long,
         val username: String,
         val firstName: String,
         val lastName: String
--- app/src/main/java/ru/romanov/tonkoslovie/ui/screens/profile/ProfileActivity.kt
+++ app/src/main/java/ru/romanov/tonkoslovie/ui/screens/profile/ProfileActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import android.support.design.widget.TextInputEditText
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import ru.romanov.tonkoslovie.R
 import ru.romanov.tonkoslovie.data.api.ApiCreator
 
-class ProfileActivity  : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +26,9 @@ class ProfileActivity  : AppCompatActivity() {
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
                         { result ->
+                            val userNameInput = findViewById<TextInputEditText>(R.id.profile_input_username)
+                            userNameInput.setText(result.username)
+
                             val firstNameInput = findViewById<TextInputEditText>(R.id.profile_input_firstName)
                             firstNameInput.setText(result.firstName)
 
